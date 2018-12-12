2e4d45eb987c46452b0f5a0bc75d10742843597f
xiaosong.xiong <xiaosong.xiong@hotmail.com>
2018-12-12 22:28:07 +0800

    修改数据库的账号和密码

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'homestead',
+    'password' => 'secret',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
